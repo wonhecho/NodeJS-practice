972c08362fb2277f07fb54ffbceefce6ec60385a
WonHeeCho <50203463+wonhecho@users.noreply.github.com>
2021-12-20 18:19:21 +0900

    Update app.js

--- mongo/app.js
+++ mongo/app.js
@@ -38,5 +38,5 @@ app.use((err, req, res, next)=>{
   res.render('error');
 });
 app.listen(app.get('port'),()=>{
-  console.log(app.get('port'), '번 포트에서 대기중');
+  console.log(app.get('port'), '번 포트에서 대기중'); //waiting
 })
