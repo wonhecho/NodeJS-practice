c377a3fbf01f40505787664de82875961d83ed4a
WonHeeCho <50203463+wonhecho@users.noreply.github.com>
2022-01-06 22:36:23 +0900

    Update Basic.js

--- Server/Basic.js
+++ Server/Basic.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req,res) => {
-  res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
+  res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});      //기초 서버 만드는 부분
   res.write('<h1>Node JS </h1>');
   })
   .listen(8080,() => {
