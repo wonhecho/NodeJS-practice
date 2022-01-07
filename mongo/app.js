eb359835e8c21af65d9ef4783f529fc3946f1ecb
WonHeeCho <50203463+wonhecho@users.noreply.github.com>
2022-01-07 23:58:25 +0900

    Update app.js

--- mongo/app.js
+++ mongo/app.js
@@ -19,7 +19,7 @@ connect();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,'public')));   //public 확인
-app.use(express.json());
+app.use(express.json());                                  //join
 app.use(express.urlencoded({ extended: false}));
 
 app.use('/',indexRouter);
